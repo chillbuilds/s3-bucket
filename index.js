bfc3a888e5f0eaa93c6f4fd162c26ef16628c489
heliumface770 <heliumface770@gmail.com>
2020-04-05 13:19:41 -0600

    added file delete function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bucketPush = require('./modules/bucketPush')
 const bucketPull = require('./modules/bucketPull')
 const listObj = require('./modules/listObj')
+const deleteFile = require('./modules/deleteFile')
 const opn = require('opn');
 const inquirer = require('inquirer')
 const fs  = require('fs')
@@ -12,17 +13,18 @@ async function startPrompt(){
     inquirer.prompt (
         {type: 'list',
         name: 'choice',
-        choices: ['Push to Bucket', 'Pull From Bucket', 'Delete From Bucket', 'List Objects'],
+        choices: ['Push To Bucket', 'Pull From Bucket', 'Delete From Bucket', 'List Objects'],
         message: 'What would you like to do?'}
     ).then(function(data){
         switch(data.choice) {
-            case 'Push to Bucket':
+            case 'Push To Bucket':
                 pushPrompt()
                 break;
             case 'Pull From Bucket':
                 pullPrompt()
                 break;
-            case 'Delete from Bucket':
+            case 'Delete From Bucket':
+                deletePrompt()
                 break;
             case 'List Objects':
                 listObjects()
@@ -78,6 +80,36 @@ function listObjects(){
     }
     console.log(keyArr)
     startPrompt()
+}
+function deletePrompt(){
+    let keyArr = []
+    for(var i = 0; i < bucketList.length;i++){
+        keyArr.push(bucketList[i].Key)
+    }
+    inquirer.prompt (
+        {type: 'list',
+        name: 'file',
+        choices: keyArr,
+        message: 'Select File'}
+    ).then(function(data){
+        switch(data.file) {
+            case 'Image/':
+                startPrompt()
+                break;
+            case 'Misc/':
+                startPrompt()
+                break;
+            case 'Text/':
+                startPrompt()
+                break;
+            case 'Video/':
+                startPrompt()
+                break;
+            default:
+                deleteFile(keyDataObj, data.file)
+                startPrompt()
+                break;
+    }})
 }
 
 startPrompt()
--- modules/deleteFile.js
+++ modules/deleteFile.js
@@ -1,3 +1,17 @@
-module.exports =  function deleteFile(keyDataObj, pullObj){
+module.exports =  function deleteFile(keyDataObj, file){
     const AWS = require('aws-sdk');
+
+    //configuring the AWS environment
+    AWS.config.update({
+        accessKeyId: keyDataObj.accessKeyId,
+        secretAccessKey: keyDataObj.secretAccessKey
+      });
+    var s3 = new AWS.S3();
+    var params = {  Bucket: keyDataObj.bucket, Key: file };
+
+    s3.deleteObject(params, function(err, data) {
+        if (err) console.log(err, err.stack)  // error
+        else console.log("\n\nFile Removed From Bucket")  // deleted
+    })
+    return
 }
